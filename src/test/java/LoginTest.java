357a09c9dc20f31015243cf4818aab83f0394d50
Fatema <rfatema2010@gmail.com>
2020-07-07 17:52:34 -0500

    3rd commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,7 +17,7 @@ public class LoginTest {
 	driver= new FirefoxDriver();	
 	}
 	
-	
+	@Test
 	public void dologin() {
 		driver.get("http://gmail.com");
 		driver.findElement(By.id("identifierId")).sendKeys("way2automation@gmail.com");
